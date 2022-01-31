844a1c6fb75af6ff425238c7aa530bfdb9d097ff
dmalenik <dmalenik@gmail.com>
2022-01-31 17:45:48 +0300

    Updates

---
+++ precache-manifest.6bee68645220f840493f3433ae711389.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f59051655a800efd9be5415fc66157b",
+    "url": "/project-5-220127-react/index.html"
+  },
+  {
+    "revision": "011af85454116294c45d",
+    "url": "/project-5-220127-react/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "29be58a1d15dfbd055d6",
+    "url": "/project-5-220127-react/static/css/main.5e6cfde1.chunk.css"
+  },
+  {
+    "revision": "011af85454116294c45d",
+    "url": "/project-5-220127-react/static/js/2.5f6c7f12.chunk.js"
+  },
+  {
+    "revision": "9ac7d1fb223f2f9a1e9e",
+    "url": "/project-5-220127-react/static/js/3.79a04dbb.chunk.js"
+  },
+  {
+    "revision": "29be58a1d15dfbd055d6",
+    "url": "/project-5-220127-react/static/js/main.789ea619.chunk.js"
+  },
+  {
+    "revision": "da43fe107bf8bdeea768",
+    "url": "/project-5-220127-react/static/js/runtime~main.f81b15fd.js"
+  },
+  {
+    "revision": "15003570128cd24165ee939419817fea",
+    "url": "/project-5-220127-react/static/media/birmingham-museums-trust-Kv1hYl9LlxU-unsplash-imresizer.15003570.jpg"
+  },
+  {
+    "revision": "4a29e389f4466816b28aa9bb43ee6438",
+    "url": "/project-5-220127-react/static/media/birmingham-museums-trust-zTJX8ObJdp4-unsplash-imresizer.4a29e389.jpg"
+  },
+  {
+    "revision": "a1f2dcc43f76b3f97fdeae8597d20f5d",
+    "url": "/project-5-220127-react/static/media/dan-otis-OYFHT4X5isg-unsplash-imresizer.a1f2dcc4.jpg"
+  }
+]);
